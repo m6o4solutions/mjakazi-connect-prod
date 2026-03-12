60af6b10e5bdd1197d024f6e0a19e1861d68a860
Michael Owuor <michael.owuor@gmail.com>
2026-03-12 06:14:06 +0300

    chore(config): add ngrok dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	allowedDevOrigins: ["nella-nonexcepting-emil.ngrok-free.app"],
 	experimental: {
 		workerThreads: false,
 	},
