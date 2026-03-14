b3afdd6818a7e346961554c5923a01414e19ca24
Michael Owuor <michael.owuor@gmail.com>
2026-03-14 13:43:14 +0300

    feat(auth): add admin and sa dashboard pages and routing

---
+++ src/app/(saas)/dashboard/admin/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+
+const Page = () => {
+	return (
+		<div>
+			<div>Admin Dashboard</div>
+			<SignOutButton>
+				<Button variant="outline">Sign Out</Button>
+			</SignOutButton>
+		</div>
+	);
+};
+
+export { Page as default };
