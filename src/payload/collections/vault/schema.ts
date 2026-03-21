df5aa96216233de749a893a27c027c930d785d51
Michael Owuor <michael.owuor@gmail.com>
2026-03-21 16:30:55 +0300

    feat(vault): restrict image upload mime types

--- src/payload/collections/vault/schema.ts
+++ src/payload/collections/vault/schema.ts
@@ -51,7 +51,7 @@ const Vault: CollectionConfig = {
 		},
 	],
 	// restricts file types to images and pdfs to ensure document compatibility
-	upload: { mimeTypes: ["image/*", "application/pdf"] },
+	upload: { mimeTypes: ["application/pdf", "image/jpeg", "image/png", "image/webp"] },
 };
 
 export { Vault };
